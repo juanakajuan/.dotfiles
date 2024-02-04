539e793ef2769661ad003e80f16220eb5fcf03d2
Juan <juancnmireles@gmail.com>
2024-02-04 16:28:37 -0500

    Add .Xmodmap

--- install.sh
+++ install.sh
@@ -18,10 +18,14 @@ ln -s ~/.dotfiles/nvchad-config/custom ~/.config/nvim/lua/custom
 sudo rm -rf ~/.config/tmux > /dev/null 2>&1
 ln -s ~/.dotfiles/tmux ~/.config/tmux
 
-# i3
-ln -s ~/.dotfiles/i3 ~/.config/i3
-ln -s ~/.dotfiles/i3status ~/.config/i3status
-
 # Scripts
 sudo ln -s ~/.dotfiles/scripts/tmux-sessionizer.sh /usr/local/bin/tmux-sessionizer.sh
+
+# i3
+# ln -s ~/.dotfiles/i3 ~/.config/i3
+# ln -s ~/.dotfiles/i3status ~/.config/i3status
+
+# X
+# ln -s ~/.dotfiles/.Xmodmap ~/.Xmodmap
+# ln -s ~/.dotfiles/.Xresources ~/.Xresources
 
