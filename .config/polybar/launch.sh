03414c99da8747dd6586b7d3a874dc72edc49ba8
Juan <juancnmireles@gmail.com>
2024-06-13 18:33:26 -0400

    Remove q: keybind in nvim

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
 polybar --config=/home/juanix/.config/polybar/config.ini bar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=/home/juanix/.config/polybar/config.ini bottom_bar 2>&1 | tee -a /tmp/polybar1.log & disown
 echo "Bars launched..."
