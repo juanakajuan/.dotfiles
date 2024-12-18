38362520c41ec572c85a8bc60ad9d6f632d0c90f
Juan Mireles <jmireles@torrentialdata.com>
2024-12-18 16:10:37 -0500

    Remove ssh copying

--- .config/nvim/lua/juanakajuan/options.lua
+++ .config/nvim/lua/juanakajuan/options.lua
@@ -24,17 +24,6 @@ vim.opt.mouse = "a"
 --  See `:help 'clipboard'`
 vim.schedule(function()
     vim.opt.clipboard = "unnamedplus"
-    vim.g.clipboard = {
-        name = "OSC 52",
-        copy = {
-            ["+"] = require("vim.ui.clipboard.osc52").copy,
-            ["*"] = require("vim.ui.clipboard.osc52").copy,
-        },
-        paste = {
-            ["+"] = require("vim.ui.clipboard.osc52").paste,
-            ["*"] = require("vim.ui.clipboard.osc52").paste,
-        },
-    }
 end)
 
 -- Enable break indent
