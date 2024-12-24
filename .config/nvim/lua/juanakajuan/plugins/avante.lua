422e3a2ce9e62417ce5c04ba6b83d2f5cd539176
Juan Mireles <juanmireles@proton.me>
2024-12-24 12:14:28 -0500

    Add toggle keymap to avante

--- .config/nvim/lua/juanakajuan/plugins/avante.lua
+++ .config/nvim/lua/juanakajuan/plugins/avante.lua
@@ -1,5 +1,8 @@
 return {
     "yetone/avante.nvim",
+    keys = {
+        { "<leader>at", "<CMD>AvanteToggle<CR>", desc = "Avante Toggle" },
+    },
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
