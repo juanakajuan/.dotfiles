35d97c3e9cec51bdfc28d034cb8ac33500b9edd3
Juan Mireles <juanmireles@proton.me>
2025-03-18 19:34:42 -0400

    Change colorscheme

--- .config/nvim/lua/juanakajuan/plugins/colorscheme.lua
+++ .config/nvim/lua/juanakajuan/plugins/colorscheme.lua
@@ -17,7 +17,7 @@ return {
 		vim.cmd [[colorscheme gruvbox]]
 
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1b1b1b" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111" })
 		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	end,
 }
