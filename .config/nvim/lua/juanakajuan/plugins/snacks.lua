dcb1dae5749c32ecf4a83a21104495dd335b8716
Juan Mireles <jmireles@torrentialdata.com>
2025-05-15 10:13:57 -0400

    Changes

--- .config/nvim/lua/juanakajuan/plugins/snacks.lua
+++ .config/nvim/lua/juanakajuan/plugins/snacks.lua
@@ -14,7 +14,25 @@ return {
 		picker = {
 			enabled = true,
 			layout = {
-				preset = "ivy",
+				layout = {
+					backdrop = false,
+					width = 0.8,
+					min_width = 80,
+					height = 0.9,
+					min_height = 30,
+					box = "vertical",
+					border = "rounded",
+					title = "{title} {live} {flags}",
+					title_pos = "center",
+					{ win = "input", height = 1, border = "bottom" },
+					{ win = "list", border = "none" },
+					{
+						win = "preview",
+						title = "{preview}",
+						height = 0.6,
+						border = "top",
+					},
+				},
 			},
 		},
 	},
