d8fa16a0e1ce04361e7c65ed3f96dba9c76c6076
Juan Mireles <jmireles@torrentialdata.com>
2025-05-29 16:12:09 -0400

    Add avante keymaps.

--- .config/nvim/lua/juanakajuan/keymaps.lua
+++ .config/nvim/lua/juanakajuan/keymaps.lua
@@ -71,3 +71,59 @@ vim.keymap.set("n", "<F7>", function()
 end, { noremap = true, silent = true, desc = "Toggle Spell Check" })
 
 vim.keymap.set("n", "<leader>gs", "<CMD>Neogit<CR>")
+
+local prefill_edit_window = function(request)
+	require("avante.api").edit()
+	local code_bufnr = vim.api.nvim_get_current_buf()
+	local code_winid = vim.api.nvim_get_current_win()
+	if code_bufnr == nil or code_winid == nil then
+		return
+	end
+	vim.api.nvim_buf_set_lines(code_bufnr, 0, -1, false, { request })
+	-- Optionally set the cursor position to the end of the input
+	vim.api.nvim_win_set_cursor(code_winid, { 1, #request + 1 })
+	-- Simulate Ctrl+S keypress to submit
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("<C-s>", true, true, true),
+		"v",
+		true
+	)
+end
+
+local avante_fix_diagnostics = "fix @diagnostics"
+local avante_summarize = "Summarize the following text"
+local avante_explain_code = "Explain the following code"
+local avante_add_docstring = "Add docstring to the following codes"
+local avante_fix_bugs = "Fix the bugs inside the following codes if any"
+local avante_add_tests = "Implement tests for the following code"
+local avante_optimize_code = "Optimize the following code"
+
+vim.keymap.set("v", "<leader>ad", function()
+	require("avante.api").ask {
+		question = avante_fix_diagnostics,
+	}
+end, { desc = "Fix Diagnostics" })
+
+vim.keymap.set({ "v" }, "<leader>as", function()
+	require("avante.api").ask { question = avante_summarize }
+end, { desc = "Summarize Text (ask)" })
+
+vim.keymap.set({ "v" }, "<leader>ae", function()
+	require("avante.api").ask { question = avante_explain_code }
+end, { desc = "Explain Code (ask)" })
+
+vim.keymap.set({ "v" }, "<leader>ad", function()
+	require("avante.api").ask { question = avante_add_docstring }
+end, { desc = "Add Docstring (ask)" })
+
+vim.keymap.set({ "v" }, "<leader>ab", function()
+	require("avante.api").ask { question = avante_fix_bugs }
+end, { desc = "Fix Bugs (ask)" })
+
+vim.keymap.set({ "v" }, "<leader>at", function()
+	require("avante.api").ask { question = avante_add_tests }
+end, { desc = "Add Tests (ask)" })
+
+vim.keymap.set({ "n", "v" }, "<leader>ao", function()
+	require("avante.api").ask { question = avante_optimize_code }
+end, { desc = "Optimize Code (ask)" })
