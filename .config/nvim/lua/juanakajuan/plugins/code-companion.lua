945c72ac448ba821460e5ff4e6219848cf44a7b7
Juan <juanmireles@proton.me>
2025-07-07 12:00:48 -0400

    update plugins and replace avante with code-companion

---
+++ .config/nvim/lua/juanakajuan/plugins/code-companion.lua
@@ -0,0 +1,17 @@
+return {
+	"olimorris/codecompanion.nvim",
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "anthropic",
+			},
+			inline = {
+				adapter = "anthropic",
+			},
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
