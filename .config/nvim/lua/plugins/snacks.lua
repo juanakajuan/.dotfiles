c154b13a8f68336bf4c2265747357494287629e6
Juan Mireles <jmireles@torrentialdata.com>
2025-07-24 15:17:16 -0400

    changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,32 +5,32 @@ return {
         quickfile = { enabled = true },
         terminal = { enabled = false },
         explorer = { enabled = false },
-        -- picker = {
-        --     enabled = true,
-        --     layout = {
-        --         preview = "main",
-        --         layout = {
-        --             box = "vertical",
-        --             backdrop = false,
-        --             width = 0,
-        --             height = 0.4,
-        --             position = "bottom",
-        --             border = "top",
-        --             title = " {title} {live} {flags}",
-        --             title_pos = "left",
-        --             { win = "input", height = 1, border = "bottom" },
-        --             {
-        --                 box = "horizontal",
-        --                 { win = "list", border = "none" },
-        --                 {
-        --                     win = "preview",
-        --                     title = "{preview}",
-        --                     width = 0.6,
-        --                     border = "left",
-        --                 },
-        --             },
-        --         },
-        --     },
-        -- },
+        picker = {
+            enabled = true,
+            layout = {
+                preview = "main",
+                layout = {
+                    box = "vertical",
+                    backdrop = false,
+                    width = 0,
+                    height = 0.4,
+                    position = "bottom",
+                    border = "top",
+                    title = " {title} {live} {flags}",
+                    title_pos = "left",
+                    { win = "input", height = 1, border = "bottom" },
+                    {
+                        box = "horizontal",
+                        { win = "list", border = "none" },
+                        {
+                            win = "preview",
+                            title = "{preview}",
+                            width = 0.6,
+                            border = "left",
+                        },
+                    },
+                },
+            },
+        },
     },
 }
