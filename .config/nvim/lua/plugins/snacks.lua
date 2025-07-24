7d0891967aac548d192c6080bc76162e5f162f16
Juan <juanmireles@proton.me>
2025-07-23 21:10:34 -0400

    changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,36 +1,36 @@
 return {
-  "snacks.nvim",
-  opts = {
-    bigfile = { enabled = true },
-    quickfile = { enabled = true },
-    terminal = { enabled = false },
-    explorer = { enabled = false },
-    picker = {
-      enabled = true,
-      layout = {
-        preview = "main",
-        layout = {
-          box = "vertical",
-          backdrop = false,
-          width = 0,
-          height = 0.4,
-          position = "bottom",
-          border = "top",
-          title = " {title} {live} {flags}",
-          title_pos = "left",
-          { win = "input", height = 1, border = "bottom" },
-          {
-            box = "horizontal",
-            { win = "list", border = "none" },
-            {
-              win = "preview",
-              title = "{preview}",
-              width = 0.6,
-              border = "left",
-            },
-          },
-        },
-      },
+    "snacks.nvim",
+    opts = {
+        bigfile = { enabled = true },
+        quickfile = { enabled = true },
+        terminal = { enabled = false },
+        explorer = { enabled = false },
+        -- picker = {
+        --     enabled = true,
+        --     layout = {
+        --         preview = "main",
+        --         layout = {
+        --             box = "vertical",
+        --             backdrop = false,
+        --             width = 0,
+        --             height = 0.4,
+        --             position = "bottom",
+        --             border = "top",
+        --             title = " {title} {live} {flags}",
+        --             title_pos = "left",
+        --             { win = "input", height = 1, border = "bottom" },
+        --             {
+        --                 box = "horizontal",
+        --                 { win = "list", border = "none" },
+        --                 {
+        --                     win = "preview",
+        --                     title = "{preview}",
+        --                     width = 0.6,
+        --                     border = "left",
+        --                 },
+        --             },
+        --         },
+        --     },
+        -- },
     },
-  },
 }
