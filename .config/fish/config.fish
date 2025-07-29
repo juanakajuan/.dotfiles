7c098ddd5a11ad57c750e337f57434cd74a22fda
Juan Mireles <jmireles@torrentialdata.com>
2025-07-29 10:20:18 -0400

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,21 +4,20 @@ fish_add_path ~/bin
 fish_add_path ~/.opencode/bin
 
 # Aliases
-alias azlogin 'az acr login -n tdsacr2'
-alias cd z
-alias la 'eza -lha --icons --classify=auto --group-directories-first'
-alias ls 'eza --icons --classify=auto --group-directories-first'
-alias ll 'eza -lh --icons --classify=auto --group-directories-first'
-alias lg lazygit
-alias grep 'grep --color=auto'
-alias ip 'ip --color=auto'
-alias du ncdu
-alias gs 'git status'
-alias ga 'git add'
-alias gc 'git commit -a'
-alias gp 'git push'
-alias gl 'git pull'
-alias so 'source ~/.config/fish/config.fish'
+alias azlogin="az acr login -n tdsacr2"
+alias la="eza -lha --icons --classify=auto --group-directories-first"
+alias ls="eza --icons --classify=auto --group-directories-first"
+alias ll="eza -lh --icons --classify=auto --group-directories-first"
+alias lg="lazygit"
+alias grep="grep --color=auto"
+alias ip="ip --color=auto"
+alias du="ncdu"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -a"
+alias gp="git push"
+alias gl="git pull"
+alias so="source ~/.config/fish/config.fish"
 
 # Functions
 function qnote
@@ -38,3 +37,4 @@ set -gx SUDO_EDITOR nvim
 
 starship init fish | source
 zoxide init fish | source
+alias cd="z"
