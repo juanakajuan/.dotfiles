ab4976ab91859d63b5d275d53a5b671f05abb9ef
Juan <juanmireles@proton.me>
2025-11-03 16:59:31 -0500

    changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,35 +3,33 @@ return {
     opts = {
         bigfile = { enabled = true },
         quickfile = { enabled = true },
-        -- picker = {
-        --     enabled = true,
-        --     layout = {
-        --         preview = "main",
-        --         layout = {
-        --             box = "vertical",
-        --             backdrop = false,
-        --             width = 0,
-        --             height = 0.4,
-        --             position = "bottom",
-        --             border = "top",
-        --             title = " {title} {live} {flags}",
-        --             title_pos = "left",
-        --             { win = "input", height = 1, border = "bottom" },
-        --             {
-        --                 box = "horizontal",
-        --                 { win = "list", border = "none" },
-        --                 {
-        --                     win = "preview",
-        --                     title = "{preview}",
-        --                     width = 0.6,
-        --                     border = "left",
-        --                 },
-        --             },
-        --         },
-        --     },
-        -- },
-    },
-    keys = {
-        { "<leader>ff", LazyVim.pick("smart"), desc = "Smart Find Files (Root Dir)" },
+        explorer = { enabled = false },
+        picker = {
+            enabled = true,
+            layout = {
+                preview = "main",
+                layout = {
+                    box = "vertical",
+                    backdrop = false,
+                    width = 0,
+                    height = 0.4,
+                    position = "bottom",
+                    border = "top",
+                    title = " {title} {live} {flags}",
+                    title_pos = "left",
+                    { win = "input", height = 1, border = "bottom" },
+                    {
+                        box = "horizontal",
+                        { win = "list", border = "none" },
+                        {
+                            win = "preview",
+                            title = "{preview}",
+                            width = 0.6,
+                            border = "left",
+                        },
+                    },
+                },
+            },
+        },
     },
 }
