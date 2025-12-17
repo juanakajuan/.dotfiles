0fecccbf4cd0e2e157293c99fec90f824e78a080
Juan <juanmireles@proton.me>
2025-12-17 14:15:55 -0500

    changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -14,4 +14,13 @@ opt.wrap = true -- Enable word wrap
 opt.tabstop = 4 -- Number of spaces tabs count for
 opt.shiftwidth = 4
 
-opt.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        -- vim.highlight.on_yank()
+        local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+        copy_to_unnamedplus(vim.v.event.regcontents)
+        local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+        copy_to_unnamed(vim.v.event.regcontents)
+    end,
+})
